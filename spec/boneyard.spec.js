7a4c3937e60f6f1a3c0de20817fcee9bb009e450
Gur Dotan <gurdotan@gmail.com>
2014-04-06 21:28:53 +0300

    Add collection move function

--- spec/boneyard.spec.js
+++ spec/boneyard.spec.js
@@ -1,5 +1,47 @@
 describe("Boneyard", function() {
-    it("should pass", function() {
-        expect(1).toBe(1);
+
+
+    var Model = Backbone.Model.extend();
+    var Collection = Backbone.Collection.extend({model: Model});
+
+    describe("Collecion", function() {
+
+        var collection, model, helper;
+
+        beforeEach(function() {
+            collection = new Collection([{id: "Moe"}, {id: "Larry"}, {id: "Curly"}]);
+            helper = {callback: function(){}};
+        });
+
+        it("move: should move a model in the collection to the right index", function() {
+            collection.move(collection.last(), 1);
+            expect(collection.last().id).toBe("Larry");
+            expect(collection.at(1).id).toBe("Curly");
+        });
+
+        it("move: should throw an error when moving to an illegal index", function() {
+            expect(function() {
+                collection.move(collection.last(), 3);
+            }).toThrow();
+            expect(function() {
+                collection.move(collection.last(), -1);
+            }).toThrow();
+        });
+
+        it("move: should trigger a 'reset' event on the collection", function() {
+            spyOn(helper, 'callback');
+            collection.on('reset', helper.callback);
+            collection.move(collection.last(), 1);
+            expect(helper.callback).toHaveBeenCalled();
+        });
+
+        it("move: shouldn't trigger a 'reset' event if {silent: true} options are passed", function() {
+            spyOn(helper, 'callback');
+            collection.on('reset', helper.callback);
+            collection.move(collection.last(), 1, {silent: true});
+            expect(helper.callback).not.toHaveBeenCalled();
+        });
+
     });
+
 });
---
+++ src/boneyard.collection.js
@@ -0,0 +1,50 @@
+_.extend(Backbone.Collection.prototype, {
+
+    /**
+     * Moves a model to the given index, if different from its current index. Handy
+     * for shuffling models after they've been pulled into a new position via
+     * drag and drop.
+     */
+    move : function(model, toIndex, options) {
+        (options) || (options = {});
+        if (toIndex < 0 || toIndex >= this.size()) {
+            throw new Error("Can't move model to an 'out of bounds' index")
+        }
+        var fromIndex = this.indexOf(model);
+        if (fromIndex == -1) {
+            throw new Error("Can't move a model that's not in the collection")
+        }
+        if (fromIndex !== toIndex) {
+            this.models.splice(toIndex, 0, this.models.splice(fromIndex, 1)[0]);
+            if (!options.silent) this.trigger("reset");
+        }
+    },
+
+    /**
+     * Finds the model with the given ID and removes it from a collection.
+     * Pass {fallback : "first" \ "last"} to indicate that if the model
+     * isn't found in the collection, default to removing the first \ last model
+     * @param id
+     * @param options
+     * @returns {*}
+     */
+    removeById : function(id, options) {
+        if (!_.isUndefined(id)) {
+            var model = this.get(id);
+            return this.remove(model);
+        }
+
+        if (options) {
+            if (options.fallback === "first") 	return this.remove(this.first());
+            if (options.fallback === "last") 	return this.remove(this.last());
+        }
+
+        return undefined;
+    },
+
+    getOrAdd : function(id) {
+        var model = this.get(id);
+        if (_.isUndefined(model)) this.add({id : id});
+        return this.get(id);
+    }
+});
